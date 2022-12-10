f1b21d99b577d28ed4fa38f71c3710a95fa1a362
Nyarko <nyarkoagyekum@gmail.com>
2022-12-10 14:34:33 -0800

    Printing to stderr second attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
  * main - main function body
- * printf - fuction to print to output
- * int: integer datatype
- * float: float datatype
- * char: character datatyep
- * long int: long integer datatypes
- * Return: 0 if the code runs perfectly
+ * fprintf - fuction to print to output
+ * stderr - standard error output
+ * Return: 1 if the code runs perfectly
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
+	fprintf(stderr,"and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
