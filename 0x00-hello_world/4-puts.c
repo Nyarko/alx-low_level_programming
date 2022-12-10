4d7ea9edc1bf13c9c8b26bded61d58a79f37ddd6
Nyarko <nyarkoagyekum@gmail.com>
2022-12-10 07:12:33 -0800

    Made changes to 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int puts(void) { 
-stdout ("Programming is like building a multilingual puzzle\n");
-
+int main() {
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
 }
