f6537aa300326fd8001dab2f35800b683e679d55
Nyarko <nyarkoagyekum@gmail.com>
2022-12-10 07:20:41 -0800

    Added betty requirements; commenting and indentations

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {
+/*
+ * main function contains puts functions which is an output function
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
