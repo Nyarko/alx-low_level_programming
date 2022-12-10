c067d0ace4b984ba4c0c4ec5f4d66cb117954338
Nyarko <nyarkoagyekum@gmail.com>
2022-12-10 08:10:19 -0800

    C file for using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function body
+ * printf - fuction to print to output
+ * Return: 0 if the code runs perfectly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
