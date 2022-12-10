9a5758a8f8c29ba955842929858328c6aa136db2
Nyarko <nyarkoagyekum@gmail.com>
2022-12-10 07:35:01 -0800

    More changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
- * main function contains puts functions which is an output function
+ * main -  contains puts functions which is an output function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
