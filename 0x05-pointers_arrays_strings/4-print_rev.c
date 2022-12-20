4fa510da2dbc3c514094feabe6b33cf50f4df886
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 12:53:03 -0800

    String in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * print_rev - function to print string in reverse
+ * @s: variable to be printed
+ * strrev - function from stdlib
+ * Return: no return for void function
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
