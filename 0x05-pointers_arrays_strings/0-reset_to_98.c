8825cd773d12b9457714689cce24acda1cc58c6f
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 02:54:47 -0800

    Betty doc style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /**
- * reset_to_98 - changes the value to 98 
- *
- * Return: Always 0.
+ * reset_to_98 - changes the value to 98
+ * @n: the variable to be worked with
+ * Return: void function returns nothing
  */
 void reset_to_98(int *n)
 {
