ccc5c366dfb677f6e104c8e411451f27148ee62e
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 03:26:16 -0800

    String length check 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - checks string length
+ * @s: the variable to work with
+ * Return: not a necessity for void functions
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+}
