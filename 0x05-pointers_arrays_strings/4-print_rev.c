6904b65dafbc52fb5c0e085cd29c858d5b9b1543
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 13:32:25 -0800

    Using for loop for reverse string instead now

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,11 +2,19 @@
 /**
  * print_rev - function to print string in reverse
  * @s: variable to be printed
- * strrev - function from stdlib
+ * @lastIndex: last index of the string
+ * @revEd: variable to store reversed items
+ * puts - function from stdlib
  * Return: no return for void function
  */
 
 void print_rev(char *s)
 {
-	strrev(s);
+	int lastIndex = strlen(s) - 1;
+	
+	for (lastIndex; lastIndex >= 0; LastIndex--)
+	{
+		char revEd = s[lastIndex];
+	}
+	puts(revEd);
 }
