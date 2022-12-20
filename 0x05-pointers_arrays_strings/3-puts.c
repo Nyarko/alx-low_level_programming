1834e66135e644626c4432549bca667760850858
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 03:50:05 -0800

    Puts file and function in main.h

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _puts - function to print string to stdout
+ * @str: variable to be printed to stdout
+ * puts - function from stdlib
+ * Return: no return for void function
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
