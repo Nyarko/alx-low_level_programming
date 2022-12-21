761b5e52943ea7873b962026242ca04972b6fa48
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 22:03:17 -0800

    Put in _putchar code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,20 @@
 #include <string.h>
-#include "_putchar.c"
+/*#include "_putchar.c"*/
 #include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 /**
  * print_rev - function to print string in reverse
  * @s: variable to be printed
