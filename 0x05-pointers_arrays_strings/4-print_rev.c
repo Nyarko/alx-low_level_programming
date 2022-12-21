00db3219ec853ad9c2831145118853cdae9a6cec
Nyarko <nyarkoagyekum@gmail.com>
2022-12-20 22:39:13 -0800

    Added new line variable plus another _putchar call

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,24 +4,13 @@
 int _putchar(char c);
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- *
-*int _putchar(char c)
-*{
-*	return (write(1, &c, 1));
-*}
-*/
-
-/**
  * print_rev - function to print string in reverse
  * @s: variable to be printed
  * @lastIndex: last index of the string
  * @revEd: variable to store reversed items
  * _putchar - manual function
+ * @c: the character to print
+ * @b: new line variable
  * Return: no return for void function
  */
 
@@ -29,10 +18,12 @@ void print_rev(char *s)
 {
 	char *revEd;
 	int lastIndex = strlen(s) - 1;
+	char b = '\n';
 	
 	for (; lastIndex >= 0; lastIndex--)
 	{
 		revEd = &s[lastIndex];
 		_putchar(*revEd);
 	}
+	_putchar(b);
 }
