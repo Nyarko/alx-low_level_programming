80d084dc0bf79599da006223a221b2b70d86ba9f
Nyarko <nyarkoagyekum@gmail.com>
2022-12-23 13:36:57 -0800

    First try out: concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+/**
+ * _strcat - function to concatenate strings
+ * @dLen: length of dest
+ * @sLen: length of src
+ * Return: the concatenated string is returned.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dLen = strlen(dest);
+	int sLen = strlen(dest);
+	int i;
+
+	for (i = 0; i < sLen; i++)
+	{
+		dest[dLen + i] = src[i];
+	}
+	dest[dLen + sLen + 1] = "\0";	
+}
