6dc22a0b7e4d13737f3f69178614a0a82191905a
Nyarko <nyarkoagyekum@gmail.com>
2022-12-25 11:38:25 -0800

    Reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+/**
+ * reverse_array - function to reverse an array
+ * @a: array variable
+ * @n: size variable
+ * return - no return value for void function
+ */
+
+void reverse_array(int *a, int n)
+{
+	int b[n];
+	int i;
+	int j = 0;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		b[j] = a[i];
+		if (j != 0 && j < n)
+		{
+			printf(", ");
+		}
+		printf("%d", b[j]);
+		j++;
+	}
+	printf("\n");
+}	
