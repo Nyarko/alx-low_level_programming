07d5f7c638cc9878e8db063c4056b702f936bc21
Nyarko <nyarkoagyekum@gmail.com>
2022-12-27 14:45:31 -0800

    feat: adding functional part of code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,13 +2,41 @@
 #include <time.h>
 /* more headers goes there */
 
+#include <stdio.h>
+
 /* betty style doc for function main goes there */
+
+/**
+ * main - the actual function to run
+ *
+ * Return: zero is returned
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/* your code goes there */
+	int lDig;
+	int a;
+	
+	lDig = n[(sizeof(n) - 1)];
+
+	if (lDig > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lDig);
+	}
+	else if (lDig == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lDig);
+	}
+	else if (lDig < 6 && lDig != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, lDig);
+	}
+
 	return (0);
 }
