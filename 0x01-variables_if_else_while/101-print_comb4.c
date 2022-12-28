b45e98f626d5e03ff4087d2bdf44b0a5f57650f7
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 12:06:08 -0800

    printing 3 unique combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - the overall function
+ *
+ * Return: zero is returned
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = j; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
