1c7be832404ce7ada6c027a13e43d33f6d6afa15
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 12:35:11 -0800

    printing in a pair of two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - the overall function
+ *
+ * Return: zero is returned
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				for (l = k + 1; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+
+					if (i != 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
