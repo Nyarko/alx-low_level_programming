6fb89158382cfbc796d2aab2c418565471f93102
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 03:19:15 -0800

    printing aphabets in caps and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the overall function
+ *
+ * Return: zero is returned
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c <= 90; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
