606508814f6287e2d648edb4af7f3dc77e885c0f
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 01:08:20 -0800

    feat: adding function to get last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,40 @@
 
 #include <stdio.h>
 
+int runFunc (int n);
+
 /* betty style doc for function main goes there */
+
+/**
+ * runFunc - runs checking function
+ * @n: is the variable used
+ *
+ * Return: last digit is returned
+ */
+
+int runFunc (int n)
+{
+	int divAttempts = 0;
+	int divAmount = 1;
+	int lastDig = 0;
+
+	for (divAmount; n / divAmount > 0; divAmount *= 10)
+	{
+		divAttempts++;
+	}
+
+	for (divAttempts; divAttempts > 0; divAttempts--)
+	{
+		n %= divAmount;
+		divAmount /= 10;
+
+		if (divAmount == 1)
+		{
+			lastDig = n;
+		}
+	}
+	return (lastDig);
+}	
 
 /**
  * main - the actual function to run
@@ -22,7 +55,16 @@ int main(void)
 	/* your code goes there */
 	int lDig;
 	
-	lDig = n[(sizeof(n) - 1)];
+	if (n < 0)
+	{
+		n *= -1;
+		lDig = -1 * runFunc(n);
+	}
+	else
+	{
+		lDig = runFunc(n);
+	}
+	
 
 	if (lDig > 5)
 	{
