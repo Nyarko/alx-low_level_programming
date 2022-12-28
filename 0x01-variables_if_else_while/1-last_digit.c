8f7e650be8f1edda23352cea02a2d9547e53d9c2
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 02:06:15 -0800

    fix: adding newline character

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -68,15 +68,15 @@ int main(void)
 	
 	if (lDig > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, lDig);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lDig);
 	}
 	else if (lDig == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, lDig);
+		printf("Last digit of %d is %d and is 0\n", n, lDig);
 	}
 	else if (lDig < 6 && lDig != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lDig);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lDig);
 	}
 
 	return (0);
