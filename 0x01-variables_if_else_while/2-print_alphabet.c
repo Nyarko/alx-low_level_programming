97c9ec68306c3effa23ffc1994cb4a63afb85536
Nyarko <nyarkoagyekum@gmail.com>
2022-12-28 03:07:33 -0800

    code for printing a - z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the overall function
+ *
+ * Return: zero is returned
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
