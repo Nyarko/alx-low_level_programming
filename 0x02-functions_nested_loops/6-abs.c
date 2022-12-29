195b483df66f0347412f80a079a8e89239dab5af
Nyarko <nyarkoagyekum@gmail.com>
2022-12-29 11:15:37 -0800

    checking absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function to compute absolute value
+ * @n: value to compute
+ *
+ * Return: returns computed value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
