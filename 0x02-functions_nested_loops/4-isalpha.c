4c60b7af3e1a80c93798551c3d54ad49c314fae4
Nyarko <nyarkoagyekum@gmail.com>
2022-12-29 10:11:42 -0800

    checking for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check alphabet
+ * @c: variable to work on
+ *
+ * Return: returns 1 if alphabet, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	int n = 0;
+	
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		n = 1;
+	} else
+	{
+		n = 0;
+	}
+	return (n);
+}
