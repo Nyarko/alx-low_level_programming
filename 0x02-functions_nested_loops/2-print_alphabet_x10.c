a3d67d9a539df7f66380d5da38562dbc791ea26c
Nyarko <nyarkoagyekum@gmail.com>
2022-12-29 09:23:21 -0800

    printing alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print the alphabet
+ *
+ * Return: no return value for void function"
+ */
+
+void print_alphabet(void)
+{
+	int a;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
