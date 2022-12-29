27b937b71b6c7fc99aaea846b275f18bce6ba4e0
Nyarko <nyarkoagyekum@gmail.com>
2022-12-29 08:53:42 -0800

    printing lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+int _putchar(char c);
+
+/**
+ * print_alphabet - function to print the alphabet
+ *
+ * Return: no return value for void function"
+ */
+
+void print_alphabet(void)
+{
+	int a;
+	
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}	
