3d4a5c4d416b136ce56e037539f73cae619263af
Nyarko <nyarkoagyekum@gmail.com>
2022-12-29 09:31:28 -0800

    fix: correcting function comment error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - function to print the alphabet
+ * print_alphabet_x10 - function to print the alphabet
  *
  * Return: no return value for void function"
  */
