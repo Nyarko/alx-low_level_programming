8fd810d7867607f8a3f5b5ce871bf5f765a626ff
Nyarko <nyarkoagyekum@gmail.com>
2022-12-31 15:48:37 -0800

    printing numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function to print number
+ * @n: variable to hold number
+ *
+ * Return: no value returned for void function
+ */
+
+void print_number(int n)
+{
+	int diver = 1;
+	int counter = 1;
+
+	for (; diver <= score / 10; diver *= 10)
+	{
+		counter = counter + 1;
+	}
+	for (; counter > 0; counter--)
+	{
+		n %= diver;
+		_putchar(n + 48);
+		diver /= 10;
+	}
+	putchar('\n');
+}
