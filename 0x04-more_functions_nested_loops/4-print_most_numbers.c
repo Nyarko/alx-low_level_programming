0b078a6d27735ab7d1fff78fc163a3b342664277
Nyarko <nyarkoagyekum@gmail.com>
2022-12-31 00:12:35 -0800

    printing most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers
+ *
+ * Return: no return value for void function
+ */
+
+void print_most_numbers(void)
+{
+	int c = 48;
+	/* code goes here */
+	for (;c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
