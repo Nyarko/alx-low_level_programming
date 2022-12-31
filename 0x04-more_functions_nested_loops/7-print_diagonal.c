de6beff9b4c1d532b70da6ec2650c66559762d60
Nyarko <nyarkoagyekum@gmail.com>
2022-12-31 06:49:43 -0800

    printing diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print numbers
+ * @n: number from buffer
+ *
+ * Return: no return value for void function
+ */
+
+void print_diagonal(int n)
+{
+	int c = 92;
+	int i;
+	int j;
+	/* code goes here */
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if ( i - j == 0 )
+			{
+				_putchar(c);
+			}
+			else
+			{
+				putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
