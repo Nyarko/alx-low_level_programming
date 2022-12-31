bb489dda76708b473bb20bd5bddcb756d93e5883
Nyarko <nyarkoagyekum@gmail.com>
2022-12-31 09:04:44 -0800

    printing FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - function to print numbers
+ *
+ * Return: value to return is 0
+ */
+
+int main(void)
+{
+	int i;
+	/* code goes here */
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 ==0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
