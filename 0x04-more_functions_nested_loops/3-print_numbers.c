ff80ee5c29aa2eed8dd71b79a222ee9472178b23
Nyarko <nyarkoagyekum@gmail.com>
2022-12-31 00:05:30 -0800

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ *
+ * Return: no return value for void function
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+	/* code goes here */
+	for (;c <= 57; c++)
+	{
+		_putchar(c);
+	}
+}
