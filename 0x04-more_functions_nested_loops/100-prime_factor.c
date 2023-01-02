cafa9130d1d61ab19e4f42b1037c01d49870b94b
Nyarko <nyarkoagyekum@gmail.com>
2023-01-02 12:06:02 -0800

    fix: refactoring code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,28 +15,40 @@ int main(void)
 	long int i = 1;
 	long int m = 0;
 	long int p;
+	long int myArr[10];
 	/* loop to find prime factor */
 	while (i <= n + 1)
 	{
 		/* to find factors */
 		if (n % i == 0)
 		{
-			/* to find prime factors */
-			for (p = 2; p <= sqrt(i); p++)
-			{
-				if ((i % p) == 1)
-				{
-					m = i;
-					/* assign largest factor */
-					/*if (i > m)
-					{
-						m = i;
-					}*/
-				}
-			}
+			myArr[m] = i;
+			m++;
 		}
 		i++;
 	}
-	printf("%ld\n", m);
+	for (; myArr[m] != 0; m++)
+	{
+		i = myArr[m];
+		/* to find prime factors */
+		for (p = 2; p <= sqrt(i); p++)
+		{
+			if ((i % p) == 1)
+			{
+				myArr[m] = i;
+			} else
+			{
+				myArr[m] = 0;
+			}
+		}
+	}
+	for (; myArr[m] < 10; m++)
+	{
+		if (myArr[m] < myArr[m+1])
+		{
+			myArr[m] = myArr[m + 1];
+		}
+	}
+	printf("%ld\n", myArr[m]);
 	return (0);
 }
