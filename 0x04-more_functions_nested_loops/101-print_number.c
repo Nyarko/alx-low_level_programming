0ac296a1a4aa8cc6143bc286db0e660364acba06
Nyarko <nyarkoagyekum@gmail.com>
2023-01-01 19:28:32 -0800

    fix: changing if statement for single digits

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,9 +14,8 @@ void print_number(int n)
 	int o = n;
 	int diver = 1;
 	int counter = 1;
-	
 	/* starting with printing digits below 10 */
-	if (n < 10)
+	if (n > 0 && n < 10)
 	{
 		m += n + 48;
 		_putchar(m);
@@ -29,7 +28,6 @@ void print_number(int n)
 			diver *= 10;
 			counter++;
 		}
-
 		/* code to print digits */
 		while (counter > 0)
 		{
