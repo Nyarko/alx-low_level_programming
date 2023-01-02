2256399ca1e2511eb1fa83fa556f87a26feffcdb
Nyarko <nyarkoagyekum@gmail.com>
2023-01-01 20:28:03 -0800

    fix: refactoring code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,42 +14,37 @@ void print_number(int n)
 	int o = 0;
 	int diver = 1;
 	int counter = 1;
-	/* starting with printing digits below 10 */
-	if (n > 0 && n < 10)
+	/* checking for negative input */
+	if (n < 0)
 	{
-		m += n + 48;
-		_putchar(m);
+		n *= -1;
+		o = n;
+		_putchar(45);
 	}
 	else
 	{
-		/* checking for negative input */
-		if (n < 0)
-		{
-			n *= -1;
-			o = n;
-			_putchar(45);
-		}
-		/* getting value for how many times to run */
-		while (diver <= n / 10)
-		{
-			diver *= 10;
-			counter++;
-		}
-		/* code to print digits */
-		while (counter > 0)
-		{
-			m = o / diver;
-			m += 48;
-			_putchar(m);
-			/* resetting m */
-			m -= 48;
-			/* updating 'o' for next use */
-			m *= diver;
-			m = o - m;
-			/* reducing diver for next use */
-			diver /= 10;
-			o = m;
-			counter--;
-		}
+		o = n;
+	}
+	/* getting value for how many times to run */
+	while (diver <= n / 10)
+	{
+		diver *= 10;
+		counter++;
+	}
+	/* code to print digits */
+	while (counter > 0)
+	{
+		m = o / diver;
+		m += 48;
+		_putchar(m);
+		/* resetting m */
+		m -= 48;
+		/* updating 'o' for next use */
+		m *= diver;
+		m = o - m;
+		/* reducing diver for next use */
+		diver /= 10;
+		o = m;
+		counter--;
 	}
 }
