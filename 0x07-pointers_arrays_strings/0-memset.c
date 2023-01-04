5731d53a3b55a8edce1017b1dcea80021b871bee
Nyarko <nyarkoagyekum@gmail.com>
2023-01-03 16:29:59 -0800

    emulating memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - memset similar function
+ * @s: pointer to fill
+ * @b: what to fill s with
+ * @n: number of bytes to fill with b
+ *
+ * Return: returns pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s = b;
+	}
+	return (s);
+}
